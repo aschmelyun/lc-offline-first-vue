a630ede16f986dc1c68b49bb292ad95890d836f3
Andrew Schmelyun <schmelyun@gmail.com>
2022-01-25 04:37:47 -0500

    Adds source code for Episode 2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
-
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+      <!-- Sidebar -->
+    </div>
+    <div class="flex flex-col flex-grow">
+      <!-- Main Content -->
+      <div class="flex flex-col flex-grow overflow-auto">
+        <editor-content :editor="editor" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Editor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
 export default {
   name: 'App',
+  components: {
+    EditorContent
+  },
+  data() {
+    return {
+      editor: null
+    }
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: '',
+      extensions: [
+        StarterKit
+      ],
+      editorProps: {
+        attributes: {
+          class: "prose my-6 mx-auto focus:outline-none"
+        }
+      }
+    });
+  },
+  beforeUnmount() {
+    this.editor.destroy();
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
