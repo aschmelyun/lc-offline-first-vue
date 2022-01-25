1b7f91e307d62c25faefdf092f807502b56d777b
Andrew Schmelyun <schmelyun@gmail.com>
2022-01-25 05:03:16 -0500

    Adds source code for Episode 4

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,26 @@
   <div class="flex w-screen h-screen text-gray-700">
     <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
       <!-- Sidebar -->
+      <div class="h-0 overflow-auto flex-grow">
+        <div class="mt-4">
+          <a
+            class="flex items-center h-8 text-sm pl-8 pr-3"
+            href="#"
+            v-for="note in notes"
+            :key="note.created"
+          >
+            <span class="ml-2 leading-none">{{ new Date(note.created).toLocaleString() }}</span>
+          </a>
+        </div>
+      </div>
     </div>
     <div class="flex flex-col flex-grow">
       <!-- Main Content -->
       <div class="flex flex-col flex-grow overflow-auto">
         <editor-content :editor="editor" />
+      </div>
+      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
+        <button @click="saveNote">Save Note</button>
       </div>
     </div>
   </div>
@@ -24,10 +39,13 @@ export default {
     return {
       editor: null,
       database: null,
+      notes: []
     }
   },
   async created() {
     this.database = await this.getDatabase();
+    let notes = await this.getNotes();
+    this.notes = notes.reverse();
   },
   mounted() {
     this.editor = new Editor({
@@ -48,7 +66,7 @@ export default {
   methods: {
     async getDatabase() {
       return new Promise((resolve, reject) => {
-        let db = window.indexedDB.open("notes");
+        let db = window.indexedDB.open("notes", 2);
 
         db.onerror = e => {
           reject('Error opening the database.');
@@ -61,8 +79,37 @@ export default {
 
         db.onupgradeneeded = e => {
           console.log('db.onupgradeneeded', e);
-          e.target.result.createObjectStore("notes");
+          e.target.result.deleteObjectStore("notes");
+          e.target.result.createObjectStore("notes", { keyPath: "created" });
         };
+      });
+    },
+    async saveNote() {
+      return new Promise((resolve, reject) => {
+        let transaction = this.database.transaction('notes', 'readwrite');
+        transaction.oncomplete = e => {
+          resolve();
+        }
+
+        let now = new Date();
+        let note = {
+          content: this.editor.getHTML(),
+          created: now.getTime()
+        };
+
+        this.notes.unshift(note)
+        transaction.objectStore('notes').add(note);
+      });
+    },
+    async getNotes() {
+      return new Promise((resolve, reject) => {
+        this.database.transaction('notes')
+          .objectStore('notes')
+          .getAll()
+          .onsuccess = e => {
+            console.log('getNotes()', e.target.result);
+            resolve(e.target.result);
+          };
       });
     }
   }
